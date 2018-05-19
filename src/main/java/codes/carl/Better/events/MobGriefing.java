8315149eb15352b12ea9410de39b377a8d52643e
Carl Poole <mail@carlpoole.com>
2018-05-19 19:07:37 -0400

    Disabled enderman griefing

--- src/main/java/codes/carl/Better/events/MobGriefing.java
+++ src/main/java/codes/carl/Better/events/MobGriefing.java
@@ -1,10 +1,12 @@
 package codes.carl.Better.events;
 
 import org.bukkit.entity.Creeper;
+import org.bukkit.entity.Enderman;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Fireball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class MobGriefing implements Listener {
@@ -26,6 +28,16 @@ public class MobGriefing implements Listener {
             event.blockList().clear();
         }
 
+    }
+
+    @EventHandler
+    public void onEntityChangeBlock(EntityChangeBlockEvent entityChangeBlockEvent) {
+        Entity entity = entityChangeBlockEvent.getEntity();
+
+        // Stop Endermen picking up blocks
+        if(entity instanceof Enderman) {
+            entityChangeBlockEvent.setCancelled(true);
+        }
     }
 
 }
